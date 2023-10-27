be0596e49e93590206bf9d0dc0ae69dcf8fd538c
walylo <106744032+aninhawksk@users.noreply.github.com>
2023-10-27 09:36:34 -0300

    js

---
+++ jogo.js
@@ -0,0 +1,16 @@
+const celulas = document.querySelector(".celula");
+
+let checarTurno = true;
+
+let turno; 
+
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
+
+let jogoAcabou = false;
+
+document.addEventListener ("click", (event) =>){
+    if (event.target.matches(".celula") && !jogoAcabou){
+        jogar(event.target.id);
+    }
+});
